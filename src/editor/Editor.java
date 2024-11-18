327b344f156830eab5362840ddf330af36d7fa31
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-11-17 22:42:07 -0500

    fix problema 3

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -3,43 +3,54 @@ package editor;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.ArrayList;
 
 public class Editor {
-	
+
 	public void checkSpelling(String text) {
-		ArrayList<String> errors = this.check(text);
-		if(errors != null)
+		List<String> errors = this.check(text);
+		if (errors.size() > 0) {
 			this.displayErrors(errors);
-		else
-			this.displaySuccessMessage();
+			return;
+		}
+		this.displaySuccessMessage();
 	}
-	
-	public void displayErrors(ArrayList<String> errors) {
-		for(int i=0; i<errors.size(); i++) {
+
+	public void displayErrors(List<String> errors) {
+		for (int i = 0; i < errors.size(); i++) {
 			System.out.println("ERROR: " + errors.get(i));
 		}
 	}
-	
-	public Object displaySuccessMessage() {
+
+	public void displaySuccessMessage() {
 		System.out.println("No errors found!");
-		return null;
 	}
-	
-	public ArrayList<String> check(String text) {
-		String[] words = text.split("");
-		ArrayList<String> errors = new ArrayList<String>();
-		for(int i=0; i< words.length; i++) {
-			String[] realWords = {"foo", "bar"};
-			for(int j=0; j<realWords.length; j++) {
-				if(!words[i].equalsIgnoreCase(realWords[j])) {
-					errors.add(words[i]);
+
+	public List<String> check(String text) {
+		String[] words = text.split(" ");
+		String[] realWords = { "foo", "bar" };
+
+		List<String> errors = new ArrayList<String>();
+
+		boolean found = false;
+
+		for (int i = 0; i < words.length; i++) {
+			for (int j = 0; j < realWords.length; j++) {
+				if (words[i].equalsIgnoreCase(realWords[j])) {
+					found = true;
+					break;
 				}
 			}
+			if (!found) {
+				errors.add(words[i]);
+			}
+			found = false;
 		}
 		return errors;
+
 	}
-	
+
 	public void runEditor() {
 		System.out.println("Running editor...");
 		System.out.println("Enter text:");
@@ -52,7 +63,7 @@ public class Editor {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Editor editor = new Editor();
 		editor.runEditor();
